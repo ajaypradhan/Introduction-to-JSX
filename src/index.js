1244dcc476aa8d482924410ea3336bab13cb0746
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-07 20:47:20 +0530

    pratice

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+// ReactDOM.render(what to SharedWorker, where to show)
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
